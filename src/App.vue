5fffb47381011227b264106e97d87bc6049f41ec
chocheolhee <whcjfgml92@naver.com>
2023-08-17 21:45:01 +0900

    Vue 3.0 학습용 기록

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h2>Todo List</h2>
+
+    <input
+        class="form-control"
+        type="text"
+        v-model="searchText"
+        placeholder="Search"
+    />
+
+    <hr />
+
+    <TodoSimpleForm @add-todo="addTodo"/>
+
+    <div v-if="!filteredTodos.length">
+      There is nothing to display
+    </div>
+
+    <TodoList :todos="filteredTodos" @toggle-todo="toggleTodo" @delete-todo="deleteTodo"/>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref, computed} from "vue";
+import TodoSimpleForm from "@/components/TodoSimpleForm.vue";
+import TodoList from "@/components/TodoList.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    TodoSimpleForm,
+    TodoList
+  },
+  setup() {
+    const todos = ref([]);
+
+    const addTodo = (todo) => {
+      todos.value.push(todo);
+    };
+
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
+    };
+
+    const toggleTodo = (index) => {
+      todos.value[index].completed = !todos.value[index].completed;
+    };
+
+    const searchText = ref('');
+    const filteredTodos = computed(()=>{
+      if (searchText.value) {
+        return todos.value.filter(todo => {
+          return todo.subject.includes(searchText.value);
+        });
+      }
+
+      return todos.value;
+    })
+
+    return {
+      todos,
+      addTodo,
+      deleteTodo,
+      toggleTodo,
+      searchText,
+      filteredTodos
+    };
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
