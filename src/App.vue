1b0e83c9f20b9ce89304463ecfb8c0b75a94fe2e
chocheolhee <whcjfgml92@naver.com>
2023-08-19 10:31:55 +0900

    feat(데이터베이스 연동) axios & async / await 적용 실습

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,10 @@
         placeholder="Search"
     />
 
-    <hr />
+    <hr/>
 
     <TodoSimpleForm @add-todo="addTodo"/>
+    <div style="color: red">{{ error }}</div>
 
     <div v-if="!filteredTodos.length">
       There is nothing to display
@@ -26,6 +27,7 @@
 import {ref, computed} from "vue";
 import TodoSimpleForm from "@/components/TodoSimpleForm.vue";
 import TodoList from "@/components/TodoList.vue";
+import axios from "axios";
 
 export default {
   components: {
@@ -34,9 +36,21 @@ export default {
   },
   setup() {
     const todos = ref([]);
+    const error = ref('');
 
-    const addTodo = (todo) => {
-      todos.value.push(todo);
+    const addTodo = async (todo) => {
+      error.value = '';
+
+      try {
+        const res = await axios.post('http://localhost:3000/todos', {
+          subject: todo.subject,
+          completed: todo.completed
+        });
+        todos.value.push(res.data);
+      } catch (err) {
+        console.log(err);
+        err.value = 'SomeThing went wrong';
+      }
     };
 
     const deleteTodo = (index) => {
@@ -48,7 +62,7 @@ export default {
     };
 
     const searchText = ref('');
-    const filteredTodos = computed(()=>{
+    const filteredTodos = computed(() => {
       if (searchText.value) {
         return todos.value.filter(todo => {
           return todo.subject.includes(searchText.value);
@@ -64,7 +78,8 @@ export default {
       deleteTodo,
       toggleTodo,
       searchText,
-      filteredTodos
+      filteredTodos,
+      error
     };
   }
 }
