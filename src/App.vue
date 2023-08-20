0a4ce7340fcd7c8b1d23f57e4fb6b7d7ec99411a
chocheolhee <whcjfgml92@naver.com>
2023-08-20 19:59:53 +0900

    vue router-link 추가

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+    <router-link class="navbar-brand" to="/">
+      Home
+    </router-link>
+
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <router-link class="nav-link" to="/todos">
+          Todos
+        </router-link>
+      </li>
+    </ul>
+
+  </nav>
+
   <router-view/>
+
 </template>
 
 <script>
-
-export default {
-
-}
+export default {}
 </script>
 
 <style>
