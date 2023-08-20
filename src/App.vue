cf65a53b6f648f5433561dce142ed8165e90e53f
chocheolhee <whcjfgml92@naver.com>
2023-08-20 16:11:57 +0900

    feat(pagenation & search) 페이지네이션 및 검색기능 개선

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
         type="text"
         v-model="searchText"
         placeholder="Search"
+        @keyup.enter="searchTodo"
     />
 
     <hr/>
@@ -14,17 +15,40 @@
     <TodoSimpleForm @add-todo="addTodo"/>
     <div style="color: red">{{ error }}</div>
 
-    <div v-if="!filteredTodos.length">
+    <div v-if="!todos.length">
       There is nothing to display
     </div>
 
-    <TodoList :todos="filteredTodos" @toggle-todo="toggleTodo" @delete-todo="deleteTodo"/>
+    <TodoList :todos="todos" @toggle-todo="toggleTodo" @delete-todo="deleteTodo"/>
 
+    <hr/>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li v-if="currentPage !== 1" class="page-item">
+          <a class="page-link" @click="getTodos(currentPage - 1)" style="cursor: pointer">
+            Previous
+          </a>
+        </li>
+        <li
+            v-for="page in numberOfPages"
+            :key="page"
+            class="page-item"
+            :class="currentPage === page ? 'active' : ''"
+        >
+          <a class="page-link" @click="getTodos(page)">{{ page }}</a>
+        </li>
+        <li v-if="numberOfPages !== currentPage" class="page-item">
+          <a class="page-link" @click="getTodos(currentPage + 1)" style="cursor: pointer">
+            Next
+          </a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
 <script>
-import {ref, computed} from "vue";
+import {ref, computed, watch} from "vue";
 import TodoSimpleForm from "@/components/TodoSimpleForm.vue";
 import TodoList from "@/components/TodoList.vue";
 import axios from "axios";
@@ -37,10 +61,20 @@ export default {
   setup() {
     const todos = ref([]);
     const error = ref('');
+    const numberOfTodos = ref(0);
+    const currentPage = ref(1);
+    const limit = 5;
+    const searchText = ref('');
 
-    const getTodos = async () => {
+    const numberOfPages = computed(() => {
+      return Math.ceil(numberOfTodos.value / limit);
+    })
+
+    const getTodos = async (page = currentPage.value) => {
+      currentPage.value = page;
       try {
-        const res = await axios.get('http://localhost:3000/todos/');
+        const res = await axios.get(`http://localhost:3000/todos?_sort=id&_order=desc&subject_like=${searchText.value}&_page=${page}&_limit=${limit}`);
+        numberOfTodos.value = res.headers['x-total-count'];
         todos.value = res.data;
       } catch (err) {
         console.log(err);
@@ -54,11 +88,11 @@ export default {
       error.value = '';
 
       try {
-        const res = await axios.post('http://localhost:3000/todos/', {
+        await axios.post('http://localhost:3000/todos/', {
           subject: todo.subject,
           completed: todo.completed
         });
-        todos.value.push(res.data);
+        await getTodos(1);
       } catch (err) {
         console.log(err);
         error.value = 'SomeThing went wrong';
@@ -71,7 +105,7 @@ export default {
       const id = todos.value[index].id;
       try {
         await axios.delete('http://localhost:3000/todos/' + id);
-        todos.value.splice(index, 1);
+        await getTodos(1);
       } catch (err) {
         console.log(err);
         error.value = 'Something went wrong';
@@ -91,16 +125,19 @@ export default {
       }
     };
 
-    const searchText = ref('');
-    const filteredTodos = computed(() => {
-      if (searchText.value) {
-        return todos.value.filter(todo => {
-          return todo.subject.includes(searchText.value);
-        });
-      }
+    let timeout = null;
+    const searchTodo = () => {
+      clearTimeout(timeout);
+      getTodos(1);
+    };
 
-      return todos.value;
-    })
+    watch(searchText, () => {
+      clearTimeout(timeout);
+
+      timeout = setTimeout(() => {
+        getTodos(1);
+      }, 2000)
+    });
 
     return {
       todos,
@@ -108,8 +145,11 @@ export default {
       deleteTodo,
       toggleTodo,
       searchText,
-      filteredTodos,
-      error
+      error,
+      numberOfPages,
+      currentPage,
+      getTodos,
+      searchTodo
     };
   }
 }
